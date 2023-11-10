0868d77ab4c8f1e92d498b46a3c9690bacd42687
Jason_Shih <Jason_Shih@gmail.com>
2023-11-10 13:25:58 +0800

    ============= Git Study V14=============
    Signed-off-by: Jason_Shih <Jason_Shih@gmail.com>

--- GetTest.c
+++ GetTest.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
 int main(){
-        printf("Version 13");
-        printf("ABC");
-        printf("EFG");
-        printf("HIJ");
-        printf("KLM");
-	printf("NOP");
-        printf("QRS");
-        printf("TUV");
-        printf("WXYZ");
-        printf("123");
+        printf("Version 14");
+        printf("\nToday");
 }
+
+
